33799aff01a8003c67827994f9c18e15580bdaf7
Mateusz-Nejman <mateusz_nejman@hotmail.com>
2024-08-29 12:49:40 +0200

    Flip tool

---
+++ Pixed/Tools/Transform/Flip.cs
@@ -0,0 +1,20 @@
+﻿using Pixed.Models;
+
+namespace Pixed.Tools.Transform
+{
+    internal class Flip : AbstractTransformTool
+    {
+        public override void ApplyToolOnLayer(Layer layer, bool altKey)
+        {
+            TransformUtils.Axis axis = TransformUtils.Axis.Vertical;
+
+            if (altKey)
+            {
+                axis = TransformUtils.Axis.Horizontal;
+            }
+
+            TransformUtils.Flip(ref layer, axis);
+            Subjects.RefreshCanvas.OnNext(true);
+        }
+    }
+}
