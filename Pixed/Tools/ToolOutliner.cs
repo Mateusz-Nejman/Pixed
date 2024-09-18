52f9b3920910a49756a56161c4ecf11c8e2414f9
Mateusz-Nejman <mateusz_nejman@hotmail.com>
2024-09-18 08:58:06 +0200

    Outliner Tool

---
+++ Pixed/Tools/ToolOutliner.cs
@@ -0,0 +1,20 @@
+﻿using Pixed.Input;
+using Pixed.Models;
+using Pixed.Utils;
+using System.Drawing;
+
+namespace Pixed.Tools
+{
+    internal class ToolOutliner : BaseTool
+    {
+        public override void ApplyTool(int x, int y, Frame frame, ref Bitmap overlay)
+        {
+            bool fillCorners = Keyboard.Modifiers.HasFlag(Avalonia.Input.KeyModifiers.Control);
+            var color = GetToolColor();
+            var entry = PaintUtils.OutlineSimiliarConnectedPixels(frame.Layers[frame.SelectedLayer], x, y, color, fillCorners);
+            entry.FrameId = frame.Id;
+            Global.CurrentModel.AddHistory(entry.ToEntry());
+            Subjects.RefreshCanvas.OnNext(entry);
+        }
+    }
+}
