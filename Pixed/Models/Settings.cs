2f663a4a4e5a98962f1fa2efcf54af1a4cef62a1
Mateusz Nejman <mateusz_nejman@hotmail.com>
2024-09-22 18:40:52 +0200

    Grid settings; Show grid in Paint canvas (#30)

--- Pixed/Models/Settings.cs
+++ Pixed/Models/Settings.cs
@@ -1,7 +1,32 @@
-﻿namespace Pixed.Models;
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace Pixed.Models;
 
 internal class Settings
 {
     public int UserWidth { get; set; } = 16;
     public int UserHeight { get; set; } = 16;
+    public UniColor GridColor { get; set; } = UniColor.Black;
+    public int GridWidth { get; set; } = 1;
+    public int GridHeight { get; set; } = 1;
+    public bool GridEnabled { get; set; } = true;
+
+    public void Save()
+    {
+        string path = Path.Combine(Global.DataFolder, "settings.json");
+        File.WriteAllText(path, JsonConvert.SerializeObject(this));
+    }
+
+    public static Settings Load()
+    {
+        string path = Path.Combine(Global.DataFolder, "settings.json");
+        
+        if(File.Exists(path))
+        {
+            return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(path));
+        }
+
+        return new Settings();
+    }
 }
