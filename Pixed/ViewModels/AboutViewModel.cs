873f420c778be7ece9993c21f0ae5594ec0c0ce3
Mateusz Nejman <mateusz_nejman@hotmail.com>
2024-09-26 16:21:41 +0200

    About window changed (#52)

---
+++ Pixed/ViewModels/AboutViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Pixed.Controls;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace Pixed.ViewModels;
+internal class AboutViewModel : PixedViewModel
+{
+    public ICommand OpenUrlCommand { get; }
+    public string AppVersion { get; }
+    public AboutViewModel()
+    {
+        OpenUrlCommand = new ActionCommand<string>(OpenUrlAction);
+        AppVersion = "Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
+
+    private void OpenUrlAction(string url)
+    {
+        var psi = new ProcessStartInfo
+        {
+            FileName = url,
+            UseShellExecute = true
+        };
+        Process.Start(psi);
+    }
+}
