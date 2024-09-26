096e0e66d329661b4113a0b6c787356c4eac2b3a
Mateusz-Nejman <mateusz_nejman@hotmail.com>
2024-09-26 08:55:13 +0200

    Bresenham algorithm fixed

--- Pixed/Utils/MathUtils.cs
+++ Pixed/Utils/MathUtils.cs
@@ -9,32 +9,31 @@ internal static class MathUtils
     public static List<Point> GetBresenhamLine(int x0, int y0, int x1, int y1)
     {
         List<Point> points = [];
-        bool steep = Math.Abs(y1 - y0) > Math.Abs(x1 - x0);
-        if (steep)
-        {
-            (x0, y0) = (y0, x0);
-            (x1, y1) = (y1, x0);
-        }
-        if (x0 > x1)
-        {
-            (x0, x1) = (x1, x0);
-            (y0, y1) = (y1, y0);
-        }
 
-        int dx = x1 - x0;
-        int dy = Math.Abs(y1 - y0);
-        int error = dx / 2;
-        int ystep = (y0 < y1) ? 1 : -1;
-        int y = y0;
+        var dx = Math.Abs(x1 - x0);
+        var dy = Math.Abs(y1 - y0);
 
-        for (int x = x0; x <= x1; x++)
+        var stepX = (x0 < x1) ? 1 : -1;
+        var stepY = (y0 < y1) ? 1 : -1;
+
+        var error = dx - dy;
+
+        while(true)
         {
-            points.Add(new Point(steep ? y : x, steep ? x : y));
-            error -= dy;
-            if (error < 0)
+            points.Add(new Point(x0, y0));
+
+            if(x0 == x1 && y0 == y1) break;
+
+            var error2 = error * 2;
+            if(error2 > -dy)
             {
-                y += ystep;
+                error -= dy;
+                x0 += stepX;
+            }
+            if(error2 < dx)
+            {
                 error += dx;
+                y0 += stepY;
             }
         }
 
