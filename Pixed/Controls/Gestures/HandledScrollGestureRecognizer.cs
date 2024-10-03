e27647ba4066e4c1690ed4c0b137fd353502f830
Mateusz Nejman <mateusz_nejman@hotmail.com>
2024-10-03 21:27:24 +0200

    Colorpicker touch drag fix in scrollviewer (#70)

---
+++ Pixed/Controls/Gestures/HandledScrollGestureRecognizer.cs
@@ -0,0 +1,33 @@
+﻿using Avalonia.Input.GestureRecognizers;
+using Avalonia.Input;
+
+namespace Pixed.Controls.Gestures;
+internal class HandledScrollGestureRecognizer : ScrollGestureRecognizer
+{
+    protected override void PointerMoved(PointerEventArgs e)
+    {
+        if(e.Handled)
+        {
+            return;
+        }
+        base.PointerMoved(e);
+    }
+
+    protected override void PointerPressed(PointerPressedEventArgs e)
+    {
+        if (e.Handled)
+        {
+            return;
+        }
+        base.PointerPressed(e);
+    }
+
+    protected override void PointerReleased(PointerReleasedEventArgs e)
+    {
+        if(e.Handled)
+        {
+            return;
+        }
+        base.PointerReleased(e);
+    }
+}
