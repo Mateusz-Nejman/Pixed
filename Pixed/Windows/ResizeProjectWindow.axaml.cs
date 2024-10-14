faf7deb5a73722d1c31d30ece2362373469d613f
Mateusz Nejman <mateusz_nejman@hotmail.com>
2024-10-14 19:58:15 +0200

    Save resize data and maintain aspect ratio to settings

--- Pixed/Windows/ResizeProjectWindow.axaml.cs
+++ Pixed/Windows/ResizeProjectWindow.axaml.cs
@@ -8,27 +8,31 @@ namespace Pixed.Windows;
 
 internal partial class ResizeProjectWindow : Window
 {
-    public readonly struct ResizeResult(int width, int height, bool resizeCanvasContent, ResizeUtils.Origin anchor)
+    public readonly struct ResizeResult(int width, int height, bool resizeCanvasContent, ResizeUtils.Origin anchor, bool maintainAspectRatio)
     {
         public int Width { get; } = width;
         public int Height { get; } = height;
         public bool ResizeCanvasContent { get; } = resizeCanvasContent;
         public ResizeUtils.Origin Anchor { get; } = anchor;
+        public bool MaintainAspectRatio { get; } = maintainAspectRatio;
     }
     private readonly ResizeProjectWindowViewModel _viewModel;
+    private readonly ApplicationData _applicationData;
 
     public ResizeResult Result { get; private set; }
-    public ResizeProjectWindow(PixedModel model)
+    public ResizeProjectWindow(ApplicationData applicationData)
     {
         InitializeComponent();
+        _applicationData = applicationData;
         _viewModel = DataContext as ResizeProjectWindowViewModel;
-        _viewModel.Width = model.Width;
-        _viewModel.Height = model.Height;
+        _viewModel.Width = applicationData.CurrentModel.Width;
+        _viewModel.Height = applicationData.CurrentModel.Height;
+        _viewModel.MaintainAspectRatio = applicationData.UserSettings.MaintainAspectRatio;
     }
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
-        Result = new ResizeResult(_viewModel.Width, _viewModel.Height, _viewModel.ResizeCanvasContent, _viewModel.AnchorEnum);
+        Result = new ResizeResult(_viewModel.Width, _viewModel.Height, _viewModel.ResizeCanvasContent, _viewModel.AnchorEnum, _viewModel.MaintainAspectRatio);
         Close(true);
     }
 }
