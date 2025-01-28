a0a5a8fcb07c85613667ce65c87fd52fe408020a
Mateusz Nejman <mateusz_nejman@hotmail.com>
2025-01-28 10:38:38 +0100

    Fix writing to settings file;  Limit pallete size to 256

--- Pixed.Application/Utils/SettingsUtils.cs
+++ Pixed.Application/Utils/SettingsUtils.cs
@@ -3,6 +3,7 @@ using Pixed.Application.IO;
 using Pixed.Application.Platform;
 using Pixed.Core.Models;
 using Pixed.Core.Utils;
+using System;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,16 @@ using System.Threading.Tasks;
 namespace Pixed.Application.Utils;
 internal static class SettingsUtils
 {
+    private static readonly object _lock = new();
     public static async Task Save(IPlatformFolder platformFolder, ApplicationData applicationData)
     {
         var file = await platformFolder.GetFile("settings.json", FolderType.Root);
         var stream = await file.OpenWrite();
-        stream.Write(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(applicationData.UserSettings)));
-        stream.Dispose();
+        lock (_lock)
+        {
+            stream.Write(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(applicationData.UserSettings)));
+            stream.Dispose();
+        }
     }
 
     public static async Task<Settings> Load(IPlatformFolder platformFolder)
@@ -30,6 +35,14 @@ internal static class SettingsUtils
         Stream stream = await file.OpenRead();
         string json = stream.ReadAllText();
         stream.Dispose();
-        return JsonConvert.DeserializeObject<Settings>(json) ?? new Settings();
+
+        try
+        {
+            return JsonConvert.DeserializeObject<Settings>(json) ?? new Settings();
+        }
+        catch (Exception)
+        {
+            return new Settings();
+        }
     }
 }
