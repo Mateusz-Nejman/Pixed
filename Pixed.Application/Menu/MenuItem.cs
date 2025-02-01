257d4e63564b28d704dabf422c2c9e0ddfdc3e6d
Mateusz Nejman <mateusz_nejman@hotmail.com>
2025-02-01 16:46:57 +0100

    Remove MenuItem bitmap icon support

--- Pixed.Application/Menu/MenuItem.cs
+++ Pixed.Application/Menu/MenuItem.cs
@@ -27,15 +27,7 @@ internal class MenuItem(string? header) : IMenuItem
         if (Icon != null)
         {
             var stream = AssetLoader.Open(Icon);
-
-            if (Icon.AbsolutePath.EndsWith(".svg"))
-            {
-                icon = new SvgImage() { Source = SvgSource.LoadFromStream(stream) };
-            }
-            else
-            {
-                icon = new Bitmap(stream);
-            }
+            icon = new SvgImage() { Source = SvgSource.LoadFromStream(stream) };
             stream.Dispose();
         }
         return new Avalonia.Controls.MenuItem() { Header = Header, Command = Command, CommandParameter = CommandParameter, Icon = new Image() { Width = 16, Height = 16, Source = icon}, ItemsSource = Items != null && Items.Count > 0 ? Items.Select(i => i.ToAvaloniaMenuItem()) : null };
