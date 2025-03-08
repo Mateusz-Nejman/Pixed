d490e790b9978036dcf71af2193c8374e85e80e2
Mateusz Nejman <mateusz_nejman@hotmail.com>
2025-03-08 21:00:32 +0100

    Android Development runtime for having installed both play store version and with actual code (#219)

---
+++ Pixed.AndroidDev/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using AndroidX.Activity;
+using Avalonia.Android;
+using Avalonia;
+using Pixed.Application.DependencyInjection;
+using Pixed.Application.IO;
+using Pixed.Application.Platform;
+using Pixed.Application;
+using Avalonia.ReactiveUI;
+using AvaloniaInside.Shell;
+
+namespace Pixed.Android.Dev;
+
+[Activity(
+    Label = "Pixed - Dev",
+    Icon = "@mipmap/pixed_icon",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
+{
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        AndroidX.Core.SplashScreen.SplashScreen.InstallSplashScreen(this);
+        PlatformSettings.MainActivity = this;
+        PlatformFolder.Context = this;
+        StreamBase.StreamReadImpl = typeof(AndroidStreamRead);
+        StreamBase.StreamWriteImpl = typeof(AndroidStreamWrite);
+        PlatformDependencyRegister.Implementation = new AndroidDependencyRegister();
+        IPlatformSettings.Instance = new PlatformSettings();
+        OnBackPressedDispatcher.AddCallback(new BackPressedCallback());
+
+        base.OnCreate(savedInstanceState);
+    }
+    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
+    {
+        return base.CustomizeAppBuilder(builder)
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI()
+            .UseShell();
+    }
+}
+
+
