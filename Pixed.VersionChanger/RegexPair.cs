5ecb38b5614bf4936258d4885a9541b27bed3983
Mateusz Nejman <mateusz_nejman@hotmail.com>
2025-03-23 13:43:25 +0100

    Version changer

---
+++ Pixed.VersionChanger/RegexPair.cs
@@ -0,0 +1,7 @@
+﻿namespace Pixed.VersionChanger;
+
+public readonly struct RegexPair(string file, string pattern)
+{
+    public string File { get; } = file;
+    public string Pattern { get; } = pattern;
+}
