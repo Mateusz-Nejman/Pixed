e8b4ca6185b97938389a72e3e515c3d2b16e01ab
Mateusz-Nejman <mateusz_nejman@hotmail.com>
2025-05-12 08:15:24 +0200

    Titlebar handling fix

--- Pixed.Application/Controls/TitleBar.axaml.cs
+++ Pixed.Application/Controls/TitleBar.axaml.cs
@@ -1,6 +1,10 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
+using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
 namespace Pixed.Application.Controls;
 
@@ -37,5 +41,66 @@ internal partial class TitleBar : EmptyExtendedControl
     public TitleBar() : base()
     {
         InitializeComponent();
+
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            minimizeButton.Click += MinimizeWindow;
+            maximizeButton.Click += MaximizeWindow;
+            closeButton.Click += CloseWindow;
+            SubscribeToWindowState();
+        }
+    }
+
+    private void CloseWindow(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Window hostWindow = (Window)this.VisualRoot;
+        hostWindow.Close();
+    }
+
+    private void MaximizeWindow(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Window hostWindow = (Window)this.VisualRoot;
+
+        if (hostWindow.WindowState == WindowState.Normal)
+        {
+            hostWindow.WindowState = WindowState.Maximized;
+        }
+        else
+        {
+            hostWindow.WindowState = WindowState.Normal;
+        }
+    }
+
+    private void MinimizeWindow(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Window hostWindow = (Window)this.VisualRoot;
+        hostWindow.WindowState = WindowState.Minimized;
+    }
+
+    private async void SubscribeToWindowState()
+    {
+        Window hostWindow = (Window)this.VisualRoot;
+
+        while (hostWindow == null)
+        {
+            hostWindow = (Window)this.VisualRoot;
+            await Task.Delay(50);
+        }
+
+        hostWindow.GetObservable(Window.WindowStateProperty).Subscribe(s =>
+        {
+            if (s != WindowState.Maximized)
+            {
+                maximizeIcon.Data = Geometry.Parse("M2048 2048v-2048h-2048v2048h2048zM1843 1843h-1638v-1638h1638v1638z");
+                hostWindow.Padding = new Thickness(0, 0, 0, 0);
+                maximizeToolTip.Content = "Maximize";
+            }
+            if (s == WindowState.Maximized)
+            {
+                maximizeIcon.Data = Geometry.Parse("M2048 1638h-410v410h-1638v-1638h410v-410h1638v1638zm-614-1024h-1229v1229h1229v-1229zm409-409h-1229v205h1024v1024h205v-1229z");
+                hostWindow.Padding = new Thickness(7, 7, 7, 7);
+                maximizeToolTip.Content = "Restore Down";
+            }
+        });
     }
 }
